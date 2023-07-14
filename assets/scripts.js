b547d7a43237135f69f27f4b3ae08db8dc1ece8d
andrew bostick <andbostick@gmail.com>
2023-07-14 14:12:37 -0400

    fixed popular movie show bug

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,43 +1,74 @@
 var apiKey = "1ab627320337b55a37f95f68c3756c74";
 var movieId;
-var title;
+var searchedDiv = $('#searched')
+var popularContainer = $("#popular-container");
 
 var serachBtn = document.getElementById("search-btn");
 
 if (serachBtn) {
-  serachBtn.addEventListener("click", loadPage);
+  serachBtn.addEventListener("click", getTitle);
 }
 
-// function getTitle() {
-//   var input = document.querySelector(".input");
-//   title = input.value;
-//   console.log(title);
-//   // request();
-// }
-function loadPage() {
-  window.location.assign(
-    "results.html?q=" +
-      encodeURIComponent(document.querySelector("input.input").value)
-  );
-  getSearchParameters();
-}
-function getSearchParameters() {
-  var movieTitle = new URLSearchParams(window.location.search);
-  title = movieTitle.get("q");
+function getTitle() {
+  var input = document.querySelector(".input");
+  title = input.value;
   console.log(title);
-  request();
+  request(title);
 }
-// var trial1 = document.getElementById("trial");
+// function loadPage() {
+//   window.location.assign(
+//     "results.html?q=" +
+//       encodeURIComponent(document.querySelector("input.input").value)
+//   );
+//   getSearchParameters();
+// }
+// function getSearchParameters() {
+//   var movieTitle = new URLSearchParams(window.location.search);
+//   title = movieTitle.get("q");
+//   console.log(title);
+//   request();
+// }
+var trial1 = document.getElementById("trial");
 // function to clear input
 // trial1.innerText = "";
 
-function request() {
-  var requestCity =
+// function request(title) {
+//   var requestMovies =
+//     "https://api.themoviedb.org/3/search/multi?&language=en-US&query=" +
+//     title +
+//     "&api_key=" +
+//     apiKey;
+//   fetch(requestMovies).then(function (response) {
+//     if (response.ok) {
+//       console.log("all good");
+//       console.log(response);
+//       response.json().then(function (data) {
+//         console.log(data);
+//         if (data.results.length === 0) {
+//           console.log("error");
+//           var error1 = document.createElement("h2");
+//           error1.textContent = "Error";
+//           trial1.append(error1);
+//         } else if (data.results.length != 0) {
+//           movieId = data.results[0].id;
+//           console.log(typeof movieId);
+//           moviecredits();
+//           inputEl(data);
+//         }
+//       });
+//     } else {
+//       console.log("error");
+//     }
+//   });
+// }
+
+function request(title) {
+  var requestMovies =
     "https://api.themoviedb.org/3/search/multi?&language=en-US&query=" +
     title +
     "&api_key=" +
     apiKey;
-  fetch(requestCity).then(function (response) {
+  fetch(requestMovies).then(function (response) {
     if (response.ok) {
       console.log("all good");
       console.log(response);
@@ -45,14 +76,21 @@ function request() {
         console.log(data);
         if (data.results.length === 0) {
           console.log("error");
-          var error1 = document.createElement("h2");
-          error1.textContent = "Error";
+          var error1 = $('<h2>')
+          error1.text('Error')
           trial1.append(error1);
         } else if (data.results.length != 0) {
+          var searchedTitle = $('<h3>');
+          searchedTitle.text('Searched Movie: ' + title.toUpperCase())
+          searchedDiv.append(searchedTitle)
+          data.results.map(result => {
+            console.log(result)
+            createMovieGrid(searchedDiv, result)
+          })
           movieId = data.results[0].id;
           console.log(typeof movieId);
-          moviecredits();
-          inputEl(data);
+          // moviecredits();
+          // inputEl(data);
         }
       });
     } else {
@@ -112,15 +150,21 @@ function getPopular() {
       console.log(response);
       response.json().then(function (data) {
         console.log(data);
-        for (var i = 0; i < 12; i++) {
-          createPopularMovie(data.results[i]);
+        for (var i = 0; i < 10; i++) {
+          createMovieGrid(popularContainer, data.results[i]);
         }
       });
     }
   });
 }
-function createPopularMovie(movieData) {
-  var movieTitle = movieData.title;
+function createMovieGrid(location, movieData) {
+  if (movieData.title) {
+    var movieTitle = movieData.title;
+  }
+  if (movieData.name) {
+    var movieTitle = movieData.name;
+  }
+  
   var moviePoster = "https://image.tmdb.org/t/p/w342" + movieData.poster_path;
   var movieId = movieData.id;
   var createDiv = $("<div>");
@@ -128,8 +172,7 @@ function createPopularMovie(movieData) {
   var titleDiv = $("<div>");
   var imgTag = $("<img>");
   var pTag = $("<p>");
-  var popularContainer = $("#popular-container");
-  popularContainer.append(createDiv);
+  location.append(createDiv);
   createDiv.append(imageDiv);
   createDiv.append(titleDiv);
   imageDiv.append(imgTag);
@@ -143,4 +186,6 @@ function createPopularMovie(movieData) {
   imgTag.attr("src", moviePoster);
   console.log(movieTitle, moviePoster, movieId);
 }
-// getPopular();
+if ($("#popular-container")) {
+  getPopular();
+}
